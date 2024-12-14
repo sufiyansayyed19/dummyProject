1df6c48d03a98e44f188fb7dd43334d1e61ae52e
Sayyed Sufiyan <sufiyan.sayed92@gmail.com>
2024-12-14 14:36:46 +0530

    this is new

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>So folks that's how you create a react app</h2>
+      <h3>So that's how you do it</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
